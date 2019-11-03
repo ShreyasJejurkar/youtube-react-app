8b58851dcc4bbacf8f16810e31a6db7e00c61c00
shreyas jejurkar <shreyasjejurkar123@live.com>
2019-11-03 16:02:56 +0530

    Working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { SearchBar, VideoList, VideoDetail } from "./components";
+
+import Youtube from "./api/Youtube";
+
+export default () => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    return (
+        <Grid style={{ justifyContent: "center" }} container spacing={10}>
+            <Grid item xs={11}>
+                <Grid container spacing={10}>
+                    <Grid item xs={12}>
+                        <SearchBar onSubmit={handleSubmit} />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <VideoDetail video={selectedVideo} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+
+    async function handleSubmit(searchTerm) {
+        const { data: { items: videos } } = await Youtube.get("search", {
+            params: {
+                part: "snippet",
+                maxResults: 5,
+                key: 'AIzaSyC3O1GlUWOjmrZYIk0RFzItX3Vdi-sVPpM',
+                q: searchTerm,
+            }
+        });
+
+        setVideos(videos);
+        setSelectedVideo(videos[0]);
+    }
 }
-
-export default App;
